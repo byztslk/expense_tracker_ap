94ed04e6e075b4a67bf241a623bc7f4d8178679d
byztslk <byztslk@gmail.com>
2024-07-30 22:31:19 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+import 'package:expense_tracker_ap/expenses.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Expenses(),
+    ),
+  );
+}
